e16f4ae080ad30dbc15703f5472a6b18fc96cd99
brinem <brinemsafari@gmail.com>
2024-04-08 20:29:44 +0300

    first commit

---
+++ app/src/main/java/com/example/appsaf/ui/theme/screens/home/HomeScreen.kt
@@ -0,0 +1,26 @@
+package com.example.appsaf.ui.theme.screens.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun About() {
+
+    Column (horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxWidth()){
+        Text(text = "This is About Us Page")
+    }
+
+}
+@Preview
+@Composable
+private fun Homeprev() {
+    About()
+}
